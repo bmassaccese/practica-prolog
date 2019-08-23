8a1e5d55755667a81803708dfaca7a62ca978a0f
bmassaccese <48911373+bmassaccese@users.noreply.github.com>
2019-08-23 10:20:51 -0300

    Update el_mundo_de_la_musica.pl

--- el_mundo_de_la_musica.pl
+++ el_mundo_de_la_musica.pl
@@ -4,7 +4,6 @@
 
 disco(floydRosa, elLadoBrillanteDeLaLuna, 1000000, 1973).
 disco(tablasDeCanada, autopistaTransargentina, 500, 2006).
-disco(Artista, NombreDelDisco, Cantidad, Año).
 disco(rodrigoMalo, elCaballo, 5000000, 1999).
 disco(rodrigoMalo, loPeorDelAmor, 50000000, 1996).
 disco(rodrigoMalo, loMejorDe, 50000000, 2018).
